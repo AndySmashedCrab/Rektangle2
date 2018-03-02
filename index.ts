82df0186fb73ad3d5ccb272dccc8a6679589c78a
AndySmashedCrab <30597137+AndySmashedCrab@users.noreply.github.com>
2018-03-02 12:58:47 +0000

    In Progress

--- index.ts
+++ index.ts
@@ -1,38 +1,20 @@
-import * as path from 'path';
-import * as express from 'express';
-import * as serveIndex from 'serve-index';
-import { createServer } from 'http';
-import { Server } from 'colyseus';
-
-// Require BasicRoom handler
-import { BasicRoom } from "./rooms/Main";
-
-const port = Number(process.env.PORT || 2657);
-const app = express();
-
-// Create HTTP Server
-const httpServer = createServer(app);
-
-// Attach WebSocket Server on HTTP Server.
-const gameServer = new Server({ server: httpServer });
-
-// Register BasicRoom as "basic"
-gameServer.register("basic", BasicRoom);
-
-// Register BasicRoom with initial options, as "basic_with_options"
-// onInit(options) will receive client join options + options registered here.
-gameServer.register("basic_with_options", BasicRoom, {
-    custom_options: "you can use me on Room#onInit"
+process.on('uncaughtException', function(err) {
+    console.log('Caught exception: ' + err);
+    console.log(err.stack);
 });
 
-// Register StateHandlerRoom as "state_handler"
-//gameServer.register("state_handler", StateHandlerRoom);
+var colyseus = require('colyseus');
+var express = require('express');
+var http = require('http');
 
-// Register StateHandlerRoom as "state_handler"
-//gameServer.register("auth", AuthRoom);
+var app = express();
+var port = Number(process.env.PORT || 2657);
+var httpServer = http.createServer(app);
 
-//app.use(express.static(path.join(__dirname, "static")));
-//app.use('/', serveIndex(path.join(__dirname, "static"), {'icons': true}))
+var gameServer = new colyseus.Server({ server: httpServer });
+var Arena = require('./rooms/Main');
+
+gameServer.register('Arena', Arena)
 
 gameServer.listen(port);
 
